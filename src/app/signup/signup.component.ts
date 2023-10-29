38a9cee159a7f081787470a806d70ee907011475
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-29 22:59:24 +0530

    Added signup process

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  myform!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.myform = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      mobile: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  signup() {
+    this.http
+      .post<any>('http://localhost:3000/signup', this.myform.value)
+      .subscribe(
+        (res) => {
+          alert('You are registered Successfully !');
+          this.myform.reset();
+          this.router.navigate(['login']);
+        },
+        (error) => {
+          alert('Something went wrong!!!');
+        }
+      );
+  }
 }
