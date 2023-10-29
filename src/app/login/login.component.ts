db4fb6a2d1ccb23e8874348f92809acdafc0a1e4
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-29 23:56:03 +0530

    Add User token data

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -29,7 +29,16 @@ export class LoginComponent implements OnInit {
           res.email === this.formValue.value.email &&
           res.password === this.formValue.value.password;
         if (user) {
-          alert('User successfull Login');
+          alert('successfully logged in');
+          this.formValue.reset();
+          this.router.navigate(['restuarent']);
+          localStorage.setItem(
+            'token',
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
+          );
+          this.formValue.value.email
+            ? localStorage.setItem('usertype', 'employee')
+            : '';
         } else {
           alert('User not found with these credentials');
         }
