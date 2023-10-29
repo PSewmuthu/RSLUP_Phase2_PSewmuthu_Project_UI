641414432567264891ef536bf57fbffc44ae83d5
PSewmuthu <apasindusewmuthu@gmail.com>
2023-10-29 23:34:55 +0530

    Add Login process

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  formValue!: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
+  ngOnInit(): void {
+    this.formValue = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  login() {
+    this.http.get<any>('http://localhost:3000/signup').subscribe(
+      (res) => {
+        const user = res.find((a: any) => {
+          return (
+            a.email === this.formValue.value.email &&
+            a.password === this.formValue.value.password
+          );
+        });
+        if (user) {
+          alert('User successfull Login');
+        } else {
+          alert('User Not Found');
+        }
+      },
+      (error) => {
+        alert('Somthing Went Wrong');
+      }
+    );
+  }
 }
