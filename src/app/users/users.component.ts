9f162060c566a0d8bc4f7ab16d937cd09cf42875
PSewmuthu <apasindusewmuthu@gmail.com>
2023-11-30 03:41:27 +0530

    Integrate with user api

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -3,8 +3,85 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+  styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  loading = false;
+  profile!: Profile;
 
+  @Input()
+  session!: AuthSession;
+
+  updateProfileForm = this.formBuilder.group({
+    username: '',
+    website: '',
+    avatar_url: '',
+  });
+
+  constructor(
+    private readonly supabase: SupabaseService,
+    private formBuilder: FormBuilder,
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.getProfile();
+
+    const { username, website, avatar_url } = this.profile;
+    this.updateProfileForm.patchValue({
+      username,
+      website,
+      avatar_url,
+    });
+  }
+
+  async getProfile() {
+    try {
+      this.loading = true;
+      const { user } = this.session;
+      const { data: profile, error, status } = await this.supabase.profile(user);
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (profile) {
+        this.profile = profile;
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async updateProfile(): Promise<void> {
+    try {
+      this.loading = true;
+      const { user } = this.session;
+
+      const username = this.updateProfileForm.value.username as string;
+      const website = this.updateProfileForm.value.website as string;
+      const avatar_url = this.updateProfileForm.value.avatar_url as string;
+
+      const { error } = await this.supabase.updateProfile({
+        id: user.id,
+        username,
+        website,
+        avatar_url,
+      });
+      if (error) throw error;
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    } finally {
+      this.loading = false;
+    }
+  }
+
+  async signOut() {
+    await this.supabase.signOut();
+  }
 }
